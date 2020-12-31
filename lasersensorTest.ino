0d1297f6ca7ce3a270afe28a1c45c3faf4d6a2e6
dowel-art <65223199+dowel-art@users.noreply.github.com>
2020-12-31 22:38:45 +0900

    ソレノイド1つに変更

--- lasersensorTest.ino
+++ lasersensorTest.ino
@@ -1,13 +1,19 @@
+/*
+   Dowel
+   レーザセンサ　テスト用プログラム
+   20.12.31 Thu.
+*/
+
 #include "define.h"
 
 bool sensorState[NUM_SENSOR];
 
 void setup() {
-  pinMode(PIN_LASERSENSOR1, INPUT_PULLUP);
-  pinMode(PIN_LASERSENSOR2, INPUT_PULLUP);
-  pinMode(PIN_LASERSENSOR3, INPUT_PULLUP);
-  pinMode(PIN_LASERSENSOR4, INPUT_PULLUP);
-  Serial.begin(9600);
+  pinMode(PIN_LASERSENSOR_U, INPUT_PULLUP);
+  pinMode(PIN_LASERSENSOR_D, INPUT_PULLUP);
+  pinMode(PIN_LED_G, OUTPUT);
+  pinMode(PIN_LED_R, OUTPUT);
+  Serial.begin(BAUDRATE);
 }
 
 //センサ状態表示
@@ -19,11 +25,19 @@ void dump(bool *state) {
   Serial.println();
 }
 
+//LED出力
+void led(bool *state) {
+  if (state[0]) digitalWrite(PIN_LED_G, HIGH);
+  else digitalWrite(PIN_LED_G, LOW);
+
+  if (state[1]) digitalWrite(PIN_LED_R, HIGH);
+  else digitalWrite(PIN_LED_R, LOW);
+}
+
 void loop() {
-  sensorState[0] = digitalRead(PIN_LASERSENSOR1);
-  sensorState[1] = digitalRead(PIN_LASERSENSOR2);
-  sensorState[2] = digitalRead(PIN_LASERSENSOR3);
-  sensorState[3] = digitalRead(PIN_LASERSENSOR4);
+  sensorState[0] = digitalRead(PIN_LASERSENSOR_U);
+  sensorState[1] = digitalRead(PIN_LASERSENSOR_D);
   dump(sensorState);
+  led(sensorState);
   delay(10);
 }
